6b8e91960231f97897ddbbe3390690f205b86d69
Kiyoaki Tsurutani <kiyoaki.tsurutani@gmail.com>
2016-02-23 00:01:19 +0900

    Initial commit

---
+++ ComputerVisionAPI/App_Start/WebApiConfig.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Web.Http;
+using Microsoft.Owin.Security.OAuth;
+using Newtonsoft.Json.Serialization;
+
+namespace ComputerVisionAPI
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+            // ベアラ トークン認証のみを使用するように、Web API を設定します。
+            config.SuppressDefaultHostAuthentication();
+            config.Filters.Add(new HostAuthenticationFilter(OAuthDefaults.AuthenticationType));
+
+            // JSON データにはキャメル ケースを使用します。
+            config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
---
+++ ComputerVisionAPI/Controllers/ComputerVisionApiController.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+using System.Web.Http;
+using Microsoft.ProjectOxford.Vision;
+using Microsoft.ProjectOxford.Vision.Contract;
+
+namespace ComputerVisionAPI.Controllers
+{
+    public class ComputerVisionApiController : ApiController
+    {
+        public async Task<AnalysisResult> Get(string url)
+        {
+            var client = new VisionServiceClient("xxxxxxxxx");
+            var result = await client.AnalyzeImageAsync(url);
+            return result;
+        }
+    }
+}
